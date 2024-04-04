bb1e1d1daa5d06c23066bc426ce685de6812fb3f
JackVacek <vacekjack@gmail.com>
2024-04-04 15:20:03 -0500

    Updated website output and created pickle file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,25 @@
 import streamlit as st
+import pickle
+
+def load_model():
+    with open('linear_regression_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    return model
 
 def main():
-    st.title("My First Streamlit App")
-    st.write("Hello, world!")
+    model = load_model()
+    st.title("Model Demo")
+
+    # User inputs
+    a = st.text_input("Age:", "")
+    g = st.selectbox("Gender:", ["Male", "Female"])
+    ed = st.selectbox("Education Level:", ["High School", "Bachelor's", "Master's", "PhD"])
+    jt = st.selectbox("Job Title:",['Software Engineer Manager', 'Full Stack Engineer', 'Senior Software Engineer', 'Senior Project Engineer', 'Data Scientist', 'Back end Developer', 'Software Engineer', 'Front end Developer', 'Data Analyst', 'Product Manager', 'Web Developer', 'Marketing Coordinator', 'Sales Manager', 'Software Developer', 'Sales Associate', 'Junior Web Developer', 'Senior Data Scientist', 'Marketing Analyst', 'Financial Analyst', 'Junior Software Developer', 'Junior Software Engineer', 'Director of Data Science', 'Front End Developer', 'Senior Software Developer', 'Senior Scientist', 'Senior Data Analyst', 'Senior Manager', 'HR Manager', 'Senior Data Engineer', 'Senior UX Designer', 'Senior Engineer', 'Junior Data Analyst', 'Junior Designer', 'Senior Software Architect', 'Junior Developer', 'Senior Consultant', 'UX Designer', 'Network Engineer', 'Help Desk Analyst', 'UX Researcher', 'Director', 'IT Manager', 'VP of Operations', 'Technical Support Specialist', 'Senior IT Consultant', 'Chief Data Officer', 'Junior UX Designer', 'Data Entry Clerk', 'Senior IT Project Manager', 'Chief Technology Officer', 'Junior Data Scientist', 'Senior IT Support Specialist', 'Junior Web Designer', 'Software Manager', 'Software Project Manager', 'IT Support Specialist', 'IT Support'])
+    yoe = st.text_input("Years of Experience:","")
+    if st.button("Predict"):
+        # Make prediction using the model
+        prediction = model.predict([[int(a), g, ed, jt, int(yoe)]])[0]
+        st.write(f"Salary Prediction (Monthly in Rupees): {prediction}")
 
 if __name__ == "__main__":
     main()
